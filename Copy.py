30f3c1910ede3d8ffbf0b71022cc9b810fdcdc76
Abinator <34330638+RoaringCrown638@users.noreply.github.com>
2021-09-10 20:36:10 +0100

    Update Copy.py

--- Copy.py
+++ Copy.py
@@ -2,12 +2,13 @@ import pyperclip
 import keyboard as key
 import time
 
+directory = ""
 
 while True:
     time.sleep(0.01)
     if key.is_pressed('ctrl') and key.is_pressed('c'):
         time.sleep(0.05)
-        with open("D:\\Residual Information\\pasteList", 'a') as file:
+        with open(directory, 'a') as file:
             file.seek(2)
             file.write(pyperclip.paste())
             file.write('\n')
